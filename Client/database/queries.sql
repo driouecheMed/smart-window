2e3e1e65716a4851ea1ad8ee309fc1414f1eff82
driouechemed <drioueche.med@gmail.com>
2020-09-02 20:32:56 +0100

    code cleaning

--- Client/database/queries.sql
+++ Client/database/queries.sql
@@ -4,27 +4,21 @@
  * This File contain all database queries.
  */
 
-/* Create Database */
-/*
-	/Client/database$ sqlite3 smartwindow
-*/
 
 /* Create tables*/
-/* id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,  */
-CREATE TABLE ldr (
-                  _timestamp DATETIME, 
+
+CREATE TABLE ldr (_timestamp DATETIME, 
                   _value NUMERIC);
 /*
  * _position : UP/DOWN
  */
-CREATE TABLE motor (
-                    _timestamp DATETIME, 
+CREATE TABLE motor (_timestamp DATETIME, 
                     _position VARCHAR(4));
 /*
  * _action : UP/DOWN
  * _type : manual/automatic
  */
-CREATE TABLE log (
-                  _timestamp DATETIME, 
+CREATE TABLE log (_timestamp DATETIME, 
                   _action VARCHAR(4),
                  _type VARCHAR(9));
+
